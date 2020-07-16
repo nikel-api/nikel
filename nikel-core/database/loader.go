39feac5d4b8b056a7d4e4e68498fea0dee15a6a1
Daren Liang <daren@darenliang.com>
2020-07-16 08:41:23 -0400

    Quick fix

--- nikel-core/database/loader.go
+++ nikel-core/database/loader.go
@@ -1,7 +1,6 @@
 package database
 
 import (
-	"fmt"
 	"github.com/nikel-api/nikel/nikel-core/config"
 	"github.com/thedevsaddam/gojsonq/v2"
 	"os"
@@ -13,18 +12,10 @@ func init() {
 	pathPrefix := ""
 	wd, _ := os.Getwd()
 
-	// travel up the parent folders to find proper directory position
-	steps := 0
-	for filepath.Base(wd) != "nikel" && steps < 5 {
-
-		// exit if travelled up too far
-		if steps == 5 {
-			panic(fmt.Errorf("nikel-core: cannot find folder positions"))
-		}
-
-		pathPrefix += "../"
-		wd = filepath.Dir(wd)
-		steps += 1
+	if filepath.Base(wd) == "nikel-core" {
+		pathPrefix = "../"
+	} else if filepath.Base(wd) == "router" {
+		pathPrefix = "../../"
 	}
 
 	DB.CoursesData = gojsonq.New().File(pathPrefix + config.COURSEPATH).Reset()
